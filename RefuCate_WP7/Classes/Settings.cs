4d7b04e43799e564245cc880d404f0c47880748e
IanField90 <ian.field90@gmail.com>
2011-01-26 01:25:56 +0000

    Add more fields in WP7, Add Divorced for marital status in both.

---
+++ RefuCate_WP7/Classes/Settings.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;//property changed
+
+namespace RefuCate_WP7
+{
+    public class Settings : INotifyPropertyChanged
+    {
+        private String _userName = "";
+        public String userName
+        { 
+            get { return _userName; } 
+            set
+            {
+                _userName = value;
+                NotifyPropertyChanged("userName");
+            }
+        }
+
+
+        //Possibly SecureString?
+        private String _password;
+        public String password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyPropertyChanged("password");
+            }
+        }
+
+        private Boolean _saveSettings;
+        public Boolean saveSettings
+        {
+            get { return _saveSettings; }
+            set
+            {
+                _saveSettings = value;
+                NotifyPropertyChanged("saveDetails");
+            }
+        }
+        
+        //private String language { get; set; } // Not yet used
+
+        public void clear()
+        {
+            userName = "";
+            password = "";
+            saveSettings = false;
+            //language = "";
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (null != PropertyChanged)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
---
+++ RefuCate_WP7/Classes/Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+using System.Xml.Serialization;
+
+namespace RefuCate_WP7
+{
+    public class Utils
+    {
+        public enum LoginStatus { SUCCESS, AUTH_ERROR, CON_ERROR };
+
+        public static void clearUserSettings()
+        {
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (isf.FileExists("user.dat"))
+                {
+                    isf.DeleteFile("user.dat");
+                }
+            }
+        }
+
+        public static void saveUserSettings(Settings settings)
+        {
+            using (IsolatedStorageFile isf = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                //If user choose to save, create a new file
+                using (IsolatedStorageFileStream fs = isf.CreateFile("user.dat"))
+                {
+                    //and serialize data
+                    XmlSerializer ser = new XmlSerializer(typeof(Settings));
+                    ser.Serialize(fs, settings);
+                }
+            }
+        }
+
+        public static LoginStatus login(Settings settings)
+        {
+            //TODO: Login code + encryption
+            return LoginStatus.SUCCESS;
+        }
+
+    }
+}
