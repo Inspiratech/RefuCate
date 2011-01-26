200b724ca6165e422ca3436503fb9af85bc7cf5b
IanField90 <ian.field90@gmail.com>
2011-01-26 17:15:56 +0000

    Add some example databinding in WP7

--- RefuCate_WP7/CapturePage.xaml.cs
+++ RefuCate_WP7/CapturePage.xaml.cs
@@ -10,14 +10,17 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using RefuCate_WPF.Classes;
 
 namespace RefuCate_WP7
 {
     public partial class CapturePage : PhoneApplicationPage
     {
+        Person person = new Person();
         public CapturePage()
         {
             InitializeComponent();
+            person = (App.Current.RootVisual as PhoneApplicationFrame).DataContext as Person;
         }
     }
 }
